c7e89751bfb08076e9e627c0ecfa0e9123d9841a
seolvin21 <jangheeju0404@tukorea.ac.kr>
2023-09-20 17:57:58 +0900

    원 그리기 완성

--- character_grass.py
+++ character_grass.py
@@ -8,12 +8,16 @@ character = load_image('character.png')
 
 def run_circle():
     print('CIRCLE')
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(400, 90)
-    delay(1)
     
-    pass
+    cx, cy, r=800/2, 600/2, 200
+    for deg in range(0, 360,5):  #시작, 끝, 단위
+        x=cx+r*math.cos(deg/360*2*math.pi)
+        y=cy+r*math.sin(deg/360*2*math.pi)
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, y)
+        delay(0.01)
+
 def run_rectangle():
     print('RECTANGLE')
     pass
