7e57626f9dc323e46f00eecea0100e0721933e58
seolvin21 <jangheeju0404@tukorea.ac.kr>
2023-09-20 18:21:32 +0900

    top line move 완성

--- character_move.py
+++ character_move.py
@@ -28,15 +28,18 @@ def run_rectangle():
     print('RECTANGLE')
 
     # bottom line
-    for x in range(50,750+1,10):
-        render_all(x,90)
-        
-    # for x in range(750,50,-10):
+
+    #for x in range(50,750+1,10):
+    #    render_all(x,90)
+
+    # top line
+    for x in range(750,50-1,-10):
+        render_all(x,550)
  
     pass
 
 while True:
-    run_circle()
+    #run_circle()
     run_rectangle()
     break
 
