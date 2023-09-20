959306680cce2ceee66ca6ae9c0ac494e32524c1
seolvin21 <jangheeju0404@tukorea.ac.kr>
2023-09-20 18:15:26 +0900

    circle and bottom line, render_all def complete

--- character_move.py
+++ character_move.py
@@ -6,6 +6,14 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+
+def render_all(x,y):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.01)
+
+
 def run_circle():
     print('CIRCLE')
     
@@ -13,25 +21,22 @@ def run_circle():
     for deg in range(0, 360,5):  # 시작, 끝, 단위
         x=cx+r*math.cos(deg/360*2*math.pi)
         y=cy+r*math.sin(deg/360*2*math.pi)
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x, y)
-        delay(0.01)
+        render_all(x,y)
+        
 
 def run_rectangle():
     print('RECTANGLE')
 
     # bottom line
     for x in range(50,750+1,10):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.01)
-
+        render_all(x,90)
+        
+    # for x in range(750,50,-10):
+ 
     pass
 
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
     break
 
