a95dd031207ddc6ebb0d0b80b92790b0c27d4cd6
seolvin21 <jangheeju0404@tukorea.ac.kr>
2023-09-20 18:29:52 +0900

    rectangle 완성

--- character_move.py
+++ character_move.py
@@ -28,22 +28,20 @@ def run_rectangle():
     print('RECTANGLE')
 
     # bottom line
-
-    #for x in range(50,750+1,10):
-    #    render_all(x,90)
-
-    # top line
-    #for x in range(750,50-1,-10):
-    #   render_all(x,550)
-
+    for x in range(50,750+1,10):
+        render_all(x,90)
     #right line
-    #for y in range(90,550+1,10):
-    #   render_all(750,y)
-
-    #left line
+    for y in range(90,550+1,10):
+        render_all(750,y)
+    # top line
+    for x in range(750,50-1,-10):
+        render_all(x,550)  
+    # left line
     for y in range(550,90-1,-10):
         render_all(50,y)
-        
+    # bottom line2
+    for x in range(50,400+1,10):
+        render_all(x,90)
     pass
 
 while True:
