39dad845eba4e24559374428f74d6598b5e59c90
seolvin21 <jangheeju0404@tukorea.ac.kr>
2023-09-20 18:25:13 +0900

    right line 완성

--- character_move.py
+++ character_move.py
@@ -33,8 +33,12 @@ def run_rectangle():
     #    render_all(x,90)
 
     # top line
-    for x in range(750,50-1,-10):
-        render_all(x,550)
+    #for x in range(750,50-1,-10):
+    #   render_all(x,550)
+
+    #left line
+    for y in range(90,550+1,10):
+        render_all(750,y)
  
     pass
 
